6aaeda6a2942bdb0b469d1c26b937ba204de2400
FoxPink <aduy000@gmail.com>
2026-08-09 22:09:33 +0700

    Clarify automatic Free dependency installation

--- attachment_optimizer_pro/__manifest__.py
+++ attachment_optimizer_pro/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Attachment Optimizer Pro",
-    "version": "16.0.1.0.23",
+    "version": "16.0.1.0.24",
     "category": "Storage",
     "summary": "Automation, cleanup, routing and lifecycle for S3-compatible attachment storage.",
     "description": """
